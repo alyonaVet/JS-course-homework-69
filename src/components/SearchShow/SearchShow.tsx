a034b644f58b52d7d1e7eae03eb3e92a79c88bb9
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-18 23:19:43 +0300

    refactor SearchShow; navigate to root in changeHandler;

--- src/components/SearchShow/SearchShow.tsx
+++ src/components/SearchShow/SearchShow.tsx
@@ -2,26 +2,37 @@ import React, {useEffect, useState} from 'react';
 import {useDispatch} from 'react-redux';
 import {AppDispatch} from '../../app/store';
 import {fetchShows} from '../../containers/showsSlice';
+import {useLocation, useNavigate} from 'react-router-dom';
 
 const SearchShow = () => {
   const [searchString, setSearchString] = useState('');
 
   const dispatch: AppDispatch = useDispatch();
 
+  const navigate = useNavigate();
+  const location = useLocation();
+
   useEffect(() => {
     dispatch(fetchShows(searchString));
   }, [dispatch, searchString]);
 
+  const changeHandler = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    if (location.pathname !== '/') {
+      navigate('/');
+    }
+    setSearchString(ev.target.value);
+  };
+
   return (
     <div className="row mt-5">
-      <label htmlFor="search" className="col-sm-2 col-form-label">Search for TV Show</label>
+      <label htmlFor="search" className="col-sm-2 col-form-label fw-semibold">Search for TV Show</label>
       <div className="col-sm-10">
         <input
           className="form-control w-50"
           type="text"
           name="search"
           id="search"
-          onChange={(ev: React.ChangeEvent<HTMLInputElement>) => {setSearchString(ev.target.value)}}
+          onChange={changeHandler}
           value={searchString}
         />
       </div>
