50cbbfc2c2f5bab2dc1deb6da531aa59a5301845
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-19 10:40:28 +0300

    refactor Toolbar; added loading;

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,11 +1,18 @@
 import {NavLink} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../app/store';
 
 const Toolbar = () => {
+  const showLoading = useSelector((state: RootState) => state.show.loading);
+  const searchResultsLoading = useSelector((state: RootState) => state.shows.loading);
+
   return (
     <nav className="navbar navbar-expand-lg bg-primary bg-gradient">
       <div className="container-fluid d-flex align-items-center justify-content-between">
         <div className="d-flex align-items-center">
-          <NavLink to="/" className="navbar-brand text-white">TV Shows</NavLink>
+          <NavLink to="/" className="navbar-brand text-white">
+            TV Shows {(showLoading || searchResultsLoading) ? '- loading...' : ''}
+          </NavLink>
         </div>
       </div>
     </nav>
